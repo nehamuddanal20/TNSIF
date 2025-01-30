e921c68adce2a6e8cdace6fd946401dc9187d5ad
nehamuddanal20 <163634311+nehamuddanal20@users.noreply.github.com>
2025-01-30 22:44:07 +0530

    Add files via upload

---
+++ TNSIFDay3/Encapsulation.java
@@ -0,0 +1,17 @@
+package TNSIFDay3;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		
+		OopsConceptDemo obj = new OopsConceptDemo();
+		
+		obj.setSerialNum(101);
+		obj.setName("Neha");
+		obj.setAge(21);
+		
+		System.out.println(obj);
+
+	}
+
+}
