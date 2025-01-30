80712609dbb890bfd33d2006c32ae528ac223dde
nehamuddanal20 <163634311+nehamuddanal20@users.noreply.github.com>
2025-01-30 22:48:34 +0530

    Add files via upload

---
+++ TNSIFDay1/SimpleJava.java
@@ -0,0 +1,12 @@
+package TNSIFDay1;
+
+public class SimpleJava {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        System.out.print("Hello Students");
+    }
+}
+
+
+
