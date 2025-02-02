d6cf920010a1ceb1259be72901876ff94366a3a9
nehamuddanal20 <163634311+nehamuddanal20@users.noreply.github.com>
2025-02-02 23:28:49 +0530

    Add files via upload

---
+++ TNSIFDay6/staticblockmethod/MyClasDemo.java
@@ -0,0 +1,18 @@
+package staticblockmethod;
+
+public class MyClasDemo {
+
+	public static void main(String[] args) {
+		
+		MyClass o1 = new MyClass();
+		System.out.println(o1);
+		
+		MyClass.display(); // static method
+		
+		MyClass o2 = new MyClass();
+		System.out.println(o2);
+		MyClass.display();
+
+	}
+
+}
